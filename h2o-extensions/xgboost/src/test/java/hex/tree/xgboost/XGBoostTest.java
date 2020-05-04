d1529c86570fe5446640a428afe983178b2748f8
zuzanaolajcova <zuzana.olajcova@h2o.ai>
2020-05-04 16:21:01 +0200

    PUBDEV-7237 fixes (tests, conflicts, naming, checkpointing)

--- h2o-extensions/xgboost/src/test/java/hex/tree/xgboost/XGBoostTest.java
+++ h2o-extensions/xgboost/src/test/java/hex/tree/xgboost/XGBoostTest.java
@@ -839,6 +839,7 @@ public class XGBoostTest extends TestUtil {
       DKV.put(trainingFrameSubset);
       parms._weights_column = null;
       parms._train = trainingFrameSubset._key;
+      parms._fold_assignment = Model.Parameters.FoldAssignmentScheme.AUTO;
 
       noWeightsModel = new hex.tree.xgboost.XGBoost(parms).trainModel().get();
 
@@ -889,6 +890,8 @@ public class XGBoostTest extends TestUtil {
       DKV.put(trainingFrameSubset);
       parms._weights_column = null;
       parms._train = trainingFrameSubset._key;
+      parms._categorical_encoding = Model.Parameters.CategoricalEncodingScheme.AUTO;
+      parms._fold_assignment = Model.Parameters.FoldAssignmentScheme.AUTO;
 
       noWeightsModel = new hex.tree.xgboost.XGBoost(parms).trainModel().get();
 
@@ -937,6 +940,7 @@ public class XGBoostTest extends TestUtil {
       DKV.put(trainingFrameSubset);
       parms._weights_column = null;
       parms._train = trainingFrameSubset._key;
+      parms._fold_assignment = Model.Parameters.FoldAssignmentScheme.AUTO;
 
       noWeightsModel = new hex.tree.xgboost.XGBoost(parms).trainModel().get();
 
@@ -1317,6 +1321,8 @@ public class XGBoostTest extends TestUtil {
       denseModel = (XGBoostModel) Scope.track_generic(new hex.tree.xgboost.XGBoost(parms).trainModel().get());
       assertNotNull(denseModel);
 
+      parms._fold_assignment = Model.Parameters.FoldAssignmentScheme.AUTO;
+      parms._categorical_encoding = Model.Parameters.CategoricalEncodingScheme.AUTO;
       parms._dmatrix_type = XGBoostModel.XGBoostParameters.DMatrixType.sparse;
       sparseModel = (XGBoostModel) Scope.track_generic(new hex.tree.xgboost.XGBoost(parms).trainModel().get());
       assertNotNull(sparseModel);
@@ -1376,6 +1382,8 @@ public class XGBoostTest extends TestUtil {
       parms._backend = XGBoostModel.XGBoostParameters.Backend.cpu;
       parms._ntrees = 1;
       parms._dmatrix_type = XGBoostModel.XGBoostParameters.DMatrixType.auto;
+      parms._fold_assignment = Model.Parameters.FoldAssignmentScheme.AUTO;
+      parms._categorical_encoding = Model.Parameters.CategoricalEncodingScheme.AUTO;
 
       // Dense model utilizes fold column zero values to calculate precise memory requirements
       denseModel = (XGBoostModel) Scope.track_generic(new hex.tree.xgboost.XGBoost(parms).trainModel().get());
